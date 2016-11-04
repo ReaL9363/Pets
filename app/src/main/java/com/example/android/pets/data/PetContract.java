d391148578be8285f83666b9195e648b5035bc03
real73 <real1107055@gmail.com>
2016-11-05 01:55:31 +0600

    Contract Class created

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,34 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by ReaL PC on 11/4/2016.
+ */
+
+public final class PetContract {
+
+    public PetContract() {
+    }
+
+    public static abstract class PetsEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "pets";
+
+        public static final String COLUMN_PET_ID = BaseColumns._ID;
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_WEIGHT = "weight";
+
+
+        /**
+         * Possible values for the style of the headphone.
+         */
+        public static final int STYLE_UNKNWON = 0;
+        public static final int STYLE_MALE = 1;
+        public static final int STYLE_FEMALE = 2;
+
+
+    }
+}
