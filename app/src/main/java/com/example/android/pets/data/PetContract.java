8a83ed6188d2ea0817e95ab831a43baac9ca3497
real73 <real1107055@gmail.com>
2016-11-06 04:51:22 +0600

    UriMatcher added

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -11,31 +11,39 @@ import android.provider.BaseColumns;
  * API Contract for the Pets app.
  */
 public final class PetContract {
-    /**
-     * CONTENT_AUTHORITY
-     */
-    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
-
-    /**
-     * BASE_CONTENT_URI
-     */
-    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
-
-    /**
-     * PATH_TableName
-     */
-    public static final String PATH_PETS = "pets";
-
     // To prevent someone from accidentally instantiating the contract class,
     // give it an empty constructor.
     private PetContract() {
     }
 
     /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_PETS = "pets";
+
+    /**
      * Inner class that defines constant values for the pets database table.
      * Each entry in the table represents a single pet.
      */
     public static final class PetEntry implements BaseColumns {
+
         /**
          * The content URI to access the pet data in the provider
          */
--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -1,8 +1,11 @@
 package com.example.android.pets.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 /**
@@ -14,6 +17,29 @@ public class PetProvider extends ContentProvider {
      * Tag for the log messages
      */
     public static final String LOG_TAG = PetProvider.class.getSimpleName();
+
+    /**
+     * URI matcher code for the content URI for the pets table
+     */
+    private static final int PETS = 100;
+
+    /**
+     * URI matcher code for the content URI for a single pet in the pets table
+     */
+    private static final int PET_ID = 101;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS, PETS);
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS + "/#", PETS);
+    }
 
     /**
      * Database helper objcet
@@ -35,7 +61,24 @@ public class PetProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
-        return null;
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        Cursor cursor;
+
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                cursor = db.query(PetContract.PetEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+            case PET_ID:
+                selection = PetContract.PetEntry._ID + "#?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                cursor = db.query(PetContract.PetEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("can't contain unknown query" + uri);
+        }
+        return cursor;
     }
 
     /**
